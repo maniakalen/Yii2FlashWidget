7296ead2215d6b16299853f7d3f73a7fb9c5e2e8
Peter Georgiev <pesho10@yahoo.com>
2018-11-05 14:15:54 +0100

    Introducing active field

---
+++ src/maniakalen/widgets/ActiveField.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package maniakalen\widgets
+ * @category widgets
+ * @version 1.1.12
+ */
+
+namespace common\widgets;
+
+/**
+ * Class ActiveField
+ *
+ * ActiveField model class to provide the funcionality to filter fields by permission.
+ *
+ * In order to use in active form you need to define the field like this:
+ *
+ * $form->field($model, 'attr', [..., 'permission' => 'required-permission'])->...
+ */
+class ActiveField extends \yii\widgets\ActiveField
+{
+    public $permission;
+    public function render($content = null)
+    {
+        if (!$this->permission || \Yii::$app->user->can($this->permission)) {
+            return parent::render($content);
+        }
+
+        return '';
+    }
+}
